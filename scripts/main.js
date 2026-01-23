752671e483054b79aa6ebad71cbafd969fb3b80d
Kevin de Souza <118494682+loosebroose@users.noreply.github.com>
2026-01-22 16:05:29 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,76 @@
+---
+layout: null
+---
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link rel="stylesheet" href="/styles/main.css?v=8" />
+  <title>Kevin de Souza</title>
+</head>
+
+<body>
+  <section class="hero">
+    <div class="hero-overlay"></div>
+
+    <div class="hero-content">
+
+      <h1 class="hero-title">
+        It is in your spirit, or it is nowhere
+      </h1>
+
+      <nav class="hero-menu">
+        <a href="https://kevindesouza.substack.com" target="_blank" rel="noopener">Substack</a>
+        <a href="https://www.linkedin.com/in/kevin-de-souza-558856127" target="_blank" rel="noopener">LinkedIn</a>
+        <a href="https://github.com/loosebroose" target="_blank" rel="noopener">GitHub</a>
+      </nav>
+    </div>
+  </section>
+  <section class = "story" aria-label="Photo Gallery">
+    <div class = "story-slide" style="--img: url('images/saladita_sunset.jpeg');">
+      <div class="story-caption">
+        <h2>Saladita Sunset</h2>
+        <p>Sipping on a pacifico probably</p>
+      </div>div>
+    </div>div>
+    
+    <div class = "story-slide" style="--img: url('images/jump.jpg');">
+      <div class="story-caption">
+        <h2>Jumpin'</h2>
+        <p>Grassy mountain hops</p>
+      </div>
+    </div>
+    
+    <div class= "story-slide" style="--img: url('images/stella.jpeg');">
+      <div class="story-caption">
+        <h2>Stella Vista</h2>
+        <p>A dog in Pictou</p>
+      </div>
+    </div>
+    
+    <div class= "story-slide" style="--img: url('images/wawa.jpeg');">
+      <div class="story-caption">
+        <h2>Harry</h2>
+        <p>Fillin' up</p>
+      </div>
+    </div>
+
+    <div class= "story-slide" style="--img: url('images/moon.jpeg');">
+      <div class="story-caption">
+        <h2>Moe</h2>
+        <p>and a big moon</p>
+      </div>
+    </div>
+
+    <div class= "story-slide" style="--img: url('images/feet_pic.jpeg');">
+      <div class="story-caption">
+        <h2>My feet</h2>
+        <p>Soon to be on wiki feet</p>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
